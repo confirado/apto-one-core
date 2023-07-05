e8843afff0c44c38c39edac807916354f94cb923
ronnybaumann <ronnybaumann@users.noreply.github.com>
2023-07-05 15:16:14 +0200

    add login active or not active functionality, add logic for shop connectors (only use frontend users when no shop is connected)

--- Client/Base/frontend/components/frontend/frontend.component.ts
+++ Client/Base/frontend/components/frontend/frontend.component.ts
@@ -2,13 +2,17 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
 import { BasketService } from '@apto-base-frontend/services/basket.service';
 import { initShop } from '@apto-base-frontend/store/shop/shop.actions';
-import { Shop } from '@apto-base-frontend/store/shop/shop.model';
-import { selectShop } from '@apto-base-frontend/store/shop/shop.selectors';
+import { SelectConnector, Shop } from '@apto-base-frontend/store/shop/shop.model';
+import { selectConnector, selectShop } from '@apto-base-frontend/store/shop/shop.selectors';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { checkLoginStatus, login, logout } from '@apto-base-frontend/store/frontend-user/frontend-user.actions';
 import { selectIsLoggedIn } from '@apto-base-frontend/store/frontend-user/frontend-user.selectors';
 import { selectContentSnippet } from '@apto-base-frontend/store/content-snippets/content-snippets.selectors';
+import { environment } from '@apto-frontend/src/environments/environment';
+import { selectLocale } from '@apto-base-frontend/store/language/language.selectors';
+import { untilDestroyed } from '@ngneat/until-destroy';
+import { translate } from '@apto-base-core/store/translated-value/translated-value.model';
 
 @Component({
 	selector: 'apto-frontend',
@@ -16,15 +20,50 @@ import { selectContentSnippet } from '@apto-base-frontend/store/content-snippets
 	styleUrls: ['./frontend.component.scss'],
 })
 export class FrontendComponent implements OnInit, AfterViewInit {
-	public shop$: Observable<Shop | null>;
-  public isLoggedIn$: Observable<boolean>;
-  readonly csFrontendUsers$ = this.store.select(selectContentSnippet('plugins.frontendUsers'));
+  private locale: string = environment.defaultLocale;
+	public shop: Shop | null;
+  public connector: SelectConnector | null;
+  public isLoggedIn: boolean;
+  public loginActive: boolean;
+  public loginRequired: boolean;
 
 	@ViewChild('drawer', { static: true }) public drawer!: MatDrawer;
 
 	constructor(private store: Store, private basketService: BasketService) {
-		this.shop$ = this.store.select(selectShop);
-    this.isLoggedIn$ = this.store.select(selectIsLoggedIn);
+    this.store.select(selectLocale).subscribe((locale) => {
+      if (locale !== null) {
+        this.locale = locale;
+      }
+    });
+
+		this.store.select(selectShop).subscribe((next) => {
+      this.shop = next;
+    });
+
+    this.store.select(selectConnector).subscribe((next) => {
+      this.connector = next;
+    });
+
+    this.store.select(selectIsLoggedIn).subscribe((next) => {
+      this.isLoggedIn = next;
+    });
+
+    this.store.select(selectContentSnippet('plugins.frontendUsers.loginRequired')).subscribe((next) => {
+      if (!next) {
+        this.loginRequired = false;
+      } else {
+
+        this.loginRequired = translate(next.content, this.locale) === 'active' ? true : false;
+      }
+    });
+
+    this.store.select(selectContentSnippet('plugins.frontendUsers.loginActive')).subscribe((next) => {
+      if (!next) {
+        this.loginActive = false;
+      } else {
+        this.loginActive = translate(next.content, this.locale) === 'active' ? true : false;
+      }
+    });
 	}
 
 	ngOnInit(): void {
@@ -35,6 +74,22 @@ export class FrontendComponent implements OnInit, AfterViewInit {
 	ngAfterViewInit(): void {
 		this.basketService.sideBar = this.drawer;
 	}
+
+  accessGranted(): boolean {
+    if (!this.shop || !this.connector) {
+      return false;
+    }
+
+    if (this.connector.configured || this.loginActive === false) {
+      return true;
+    }
+
+    if (this.loginRequired === true && this.isLoggedIn === false) {
+      return false;
+    }
+
+    return true;
+  }
 
   login() {
     console.error('login');
--- Client/Base/frontend/components/header/header.component.ts
+++ Client/Base/frontend/components/header/header.component.ts
@@ -4,6 +4,11 @@ import { setLocale } from '@apto-base-frontend/store/language/language.actions';
 import { selectLanguages, selectLocale } from '@apto-base-frontend/store/language/language.selectors';
 import { environment } from '@apto-frontend/src/environments/environment';
 import { Store } from '@ngrx/store';
+import { SelectConnector } from '@apto-base-frontend/store/shop/shop.model';
+import { selectConnector } from '@apto-base-frontend/store/shop/shop.selectors';
+import { selectIsLoggedIn } from '@apto-base-frontend/store/frontend-user/frontend-user.selectors';
+import { login, logout } from '@apto-base-frontend/store/frontend-user/frontend-user.actions';
+import { translate } from '@apto-base-core/store/translated-value/translated-value.model';
 
 @Component({
 	selector: 'apto-header',
@@ -11,14 +16,74 @@ import { Store } from '@ngrx/store';
 	styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  private locale: string = environment.defaultLocale;
 	locale$ = this.store.select(selectLocale);
 	languages$ = this.store.select(selectLanguages);
 	readonly contentSnippets$ = this.store.select(selectContentSnippet('aptoLogo'));
 	mediaUrl = environment.api.media + '/';
+  public connector: SelectConnector | null;
+  public isLoggedIn: boolean;
+  public loginActive: boolean;
 
-	constructor(private store: Store) {}
+	constructor(private store: Store) {
+    this.store.select(selectLocale).subscribe((locale) => {
+      if (locale !== null) {
+        this.locale = locale;
+      }
+    });
+
+    this.store.select(selectConnector).subscribe((next) => {
+      this.connector = next;
+    });
+
+    this.store.select(selectIsLoggedIn).subscribe((next) => {
+      this.isLoggedIn = next;
+    });
+
+    this.store.select(selectContentSnippet('plugins.frontendUsers.loginActive')).subscribe((next) => {
+      if (!next) {
+        this.loginActive = false;
+      } else {
+        this.loginActive = translate(next.content, this.locale) === 'active' ? true : false;
+      }
+    });
+  }
 
 	onChangeLanguage(locale: string) {
 		this.store.dispatch(setLocale({ payload: locale }));
 	}
+
+  showLoginButton() {
+    if (this.connector && this.connector.configured === true || this.loginActive === false) {
+      return false;
+    }
+
+    if (this.isLoggedIn) {
+      return false;
+    }
+
+    return true;
+  }
+
+  showLogoutButton() {
+    if (this.connector && this.connector.configured === true || this.loginActive === false) {
+      return false;
+    }
+
+    if (!this.isLoggedIn) {
+      return false;
+    }
+
+    return true;
+  }
+
+  login() {
+    console.error('login');
+    this.store.dispatch(login({payload:{username: 'test', password: 'testtest'}}));
+  }
+
+  logout() {
+    console.error('logout');
+    this.store.dispatch(logout());
+  }
 }
