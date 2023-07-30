a562d3e70c034060e639cc18fceea07607472dad
ronnybaumann <ronnybaumann@users.noreply.github.com>
2023-07-31 00:11:45 +0200

    fix basket cart and checkout links

--- Client/Base/frontend/components/basket/basket.component.ts
+++ Client/Base/frontend/components/basket/basket.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BasketService } from '@apto-base-frontend/services/basket.service';
 import { selectFullConnector } from '@apto-base-frontend/store/shop/shop.selectors';
 import { Store } from '@ngrx/store';
+import { selectContentSnippet } from '@apto-base-frontend/store/content-snippets/content-snippets.selectors';
 
 @Component({
 	selector: 'apto-basket',
@@ -9,6 +10,8 @@ import { Store } from '@ngrx/store';
 	styleUrls: ['./basket.component.scss'],
 })
 export class BasketComponent {
+  public readonly csAptoBasket$ = this.store.select(selectContentSnippet('aptoBasket'));
+
 	public toggleSideBar(): void {
 		this.basketService.sideBar?.toggle();
 	}
