a6960d3ca97be0c75bf11ad5aa11da6fe05ab764
Arto <arto.avagyan80@gmail.com>
2023-09-02 23:15:12 +0400

    APTOONE-20: corrects just code styling

--- Client/Plugins/RequestForm/frontend/components/summary/summary.component.ts
+++ Client/Plugins/RequestForm/frontend/components/summary/summary.component.ts
@@ -1,20 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Actions, ofType } from "@ngrx/effects";
+import { Actions, ofType } from '@ngrx/effects';
 import { FormControl, FormGroup } from '@angular/forms';
-import {ViewportScroller} from "@angular/common";
+import { ViewportScroller } from '@angular/common';
 import { selectContentSnippet } from '@apto-base-frontend/store/content-snippets/content-snippets.selectors';
-import {
-  addToBasket,
-  addToBasketSuccess,
-  onError
-} from '@apto-catalog-frontend/store/configuration/configuration.actions';
-import {
-  selectConfiguration,
-  selectRenderImage,
-  selectSumPrice,
-} from '@apto-catalog-frontend/store/configuration/configuration.selectors';
+import { addToBasket, addToBasketSuccess, onError } from '@apto-catalog-frontend/store/configuration/configuration.actions';
+import { selectConfiguration, selectRenderImage, selectSumPrice } from '@apto-catalog-frontend/store/configuration/configuration.selectors';
 import { selectProduct } from '@apto-catalog-frontend/store/product/product.selectors';
 import { selectHumanReadableState } from '@apto-request-form-frontend/store/human-readable-state.selectors';
 
@@ -23,7 +15,7 @@ import { selectHumanReadableState } from '@apto-request-form-frontend/store/huma
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.scss'],
 })
-export class SummaryComponent implements OnInit{
+export class SummaryComponent implements OnInit {
   public readonly contentSnippet$ = this.store.select(selectContentSnippet('aptoSummary'));
   public readonly sidebarSummary$ = this.store.select(selectContentSnippet('sidebarSummary'));
   public readonly requestForm$ = this.store.select(selectContentSnippet('plugins.requestForm'));
@@ -37,7 +29,12 @@ export class SummaryComponent implements OnInit{
     quantityInput: new FormControl<number>(1),
   });
   public requestForm: FormGroup | null = null;
-  public requestState: { sending: boolean, success: boolean, error: boolean } = { sending: false, success: false, error: false };
+  public requestState: { sending: boolean, success: boolean, error: boolean } = {
+    sending: false,
+    success: false,
+    error: false,
+  };
+
   public constructor(private store: Store, private router: Router, activatedRoute: ActivatedRoute, private actions$: Actions, private scroller: ViewportScroller) {
     this.configuration$.subscribe((c) => {
       if (!c.loading && c.state.sections.length === 0) {
@@ -47,10 +44,10 @@ export class SummaryComponent implements OnInit{
 
     this.store.select(selectHumanReadableState).subscribe((result) => {
       this.humanReadableState = result;
-    })
+    });
   }
 
-  ngOnInit() {
+  public ngOnInit():void {
     this.actions$.pipe(ofType(onError)).subscribe((result) => {
       if (result.message.messageName === 'AddBasketConfiguration') {
         this.requestState.sending = false;
@@ -75,13 +72,13 @@ export class SummaryComponent implements OnInit{
         payload: {
           type: 'REQUEST_FORM',
           formData: this.requestForm?.value,
-          humanReadableState: this.humanReadableState
+          humanReadableState: this.humanReadableState,
         },
-      })
+      }),
     );
   }
 
-  public onRequestFormChanged(requestForm: FormGroup) {
+  public onRequestFormChanged(requestForm: FormGroup): void {
     this.requestForm = requestForm;
   }
 }
