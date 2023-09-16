534afe4492346b69219ec3b84b6feb9bfcb23182
ronnybaumann <ronnybaumann@users.noreply.github.com>
2023-09-16 04:16:33 +0200

    APTOONE-165: Bug: Renderbilder, remove debounceTime in render-image.service.ts because it seems not necessary

--- Client/Catalog/frontend/services/render-image.service.ts
+++ Client/Catalog/frontend/services/render-image.service.ts
@@ -1,5 +1,5 @@
-import { HostListener, Injectable, OnDestroy } from '@angular/core';
-import { Subject, Subscription, debounceTime } from 'rxjs';
+import { Injectable, OnDestroy } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { environment } from '@apto-frontend/src/environments/environment';
 import { RenderImageData } from '@apto-catalog-frontend/store/configuration/configuration.model';
@@ -35,15 +35,15 @@ export class RenderImageService implements OnDestroy {
     this.ngOnDestroy();
     this.subscriptions.push(
       // data for renderImage$ is sent from multiple directions, so to call all this stuff only once we put a debounce time here
-      this.renderImages$.pipe(
-        debounceTime(100)
-      ).subscribe((data) => {
+      this.renderImages$.subscribe((data) => {
         this.renderImages = data;
 
         if (this.renderImages.length > 0 && this.firstImageWidth && this.firstImageHeight) {
           this.canvas = document.createElement('canvas');
           this.ctx = this.canvas.getContext('2d');
           this.drawImages();
+        } else {
+          this.outputSrcSubject.next(null);
         }
       })
     );
