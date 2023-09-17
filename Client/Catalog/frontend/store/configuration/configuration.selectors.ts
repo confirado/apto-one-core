2034820a320a62dc419173093b84bb983b833cd7
ronnybaumann <ronnybaumann@users.noreply.github.com>
2023-09-17 12:44:02 +0200

    fix element flickering caused by reinit on value changes + code clean up

--- Client/Catalog/frontend/store/configuration/configuration.selectors.ts
+++ Client/Catalog/frontend/store/configuration/configuration.selectors.ts
@@ -257,3 +257,19 @@ export const selectElementValues = (element: Element): any =>
 	});
 
 export const selectHumanReadableState = createSelector(featureSelector, (state: CatalogFeatureState) => state.configuration.humanReadableState);
+
+export const selectCurrentProductElements = createSelector(featureSelector, (state: CatalogFeatureState) => {
+  return state.product.elements.filter((element) => element.sectionId === state.configuration.currentStep);
+});
+
+export const selectCurrentStateElements = createSelector(featureSelector, (state: CatalogFeatureState) => {
+  return state.configuration.state.elements.filter((element) => element.sectionId === state.configuration.currentStep);
+});
+
+export const selectElementState = (elementId: string) => createSelector(featureSelector, (state: CatalogFeatureState) => {
+  const filtered = state.configuration.state.elements.filter((element) => element.id === elementId);
+  if (filtered.length > 0) {
+    return filtered[0];
+  }
+  return null;
+});
