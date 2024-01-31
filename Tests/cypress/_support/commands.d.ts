ed60bf648bb29391dd545f88862cf997ceb2a3b7
Arto Avagyan <arto.avagyan80@gmail.com>
2024-01-31 20:30:58 +0400

    tests/apto-one-core adds configs for component testing, adds some example component test

--- Tests/cypress/_support/commands.d.ts
+++ Tests/cypress/_support/commands.d.ts
@@ -1,9 +1,21 @@
-namespace Cypress {
-  interface Chainable {
-    /**
-     * Custom command to select DOM element by data-cy attribute.
-     * @example cy.dataCy('greeting')
-     */
-    dataCy(value: string): Chainable<JQuery<HTMLElement>>
+import { mount } from 'cypress/angular'
+
+// Augment the Cypress namespace to include type definitions for your custom command.
+// Alternatively, can be defined in cypress/support/component.d.ts
+// with a <reference path="./component" /> at the top of your spec.
+declare global {
+  namespace Cypress {
+
+    interface Chainable {
+      mount: typeof mount
+    }
+
+    interface Chainable {
+      /**
+       * Custom command to select DOM element by data-cy attribute.
+       * @example cy.dataCy('greeting')
+       */
+      dataCy(value: string): Chainable<JQuery<HTMLElement>>
+    }
   }
 }
