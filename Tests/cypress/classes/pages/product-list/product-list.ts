f5074920edaa7bd7e8c2fd305320f63dfd7f4341
Arto Avagyan <arto.avagyan80@gmail.com>
2024-02-14 00:33:15 +0400

    tests/apto-one-core refactores product list page by moving some code to test classes

--- Tests/cypress/classes/pages/product-list/product-list.ts
+++ Tests/cypress/classes/pages/product-list/product-list.ts
@@ -2,6 +2,8 @@ import { Page } from '@apto-catalog-frontend/models/pagination';
 import { Queries } from '../../message-bus/queries';
 import { IRequestData } from '../../models';
 import { IPage } from '../../interfaces/page-interface';
+import { RequestHandler } from '../../requestHandler';
+import { Core } from '../../common/core';
 
 export interface IProductListResponse extends Page<any>{}
 
@@ -39,5 +41,85 @@ export class ProductList implements IPage {
       ...ProductList.initialCommandList,
       ...ProductList.initialCustomRequestList,
     ];
+  }
+
+  /**
+   * Searches for the given product
+   *
+   * asserts it must be found
+   *
+   * @param productName
+   */
+  public static hasProduct(productName: string): Cypress.Chainable<any> {
+
+    cy.get('apto-product-list').should('exist');
+
+    RequestHandler.registerInterceptions([Queries.FindProductsByFilter]);
+
+    cy.get('apto-search').find('.search-field input').clear().type(productName);
+
+    return cy.wait(RequestHandler.getAliasesFromRequests([Queries.FindProductsByFilter])).then(() => {
+      cy.get('.product-wrapper').should('have.length', 1);
+    });
+  }
+
+  /**
+   * Searches for the given product
+   *
+   * asserts it must not be found
+   *
+   * @param productName
+   */
+  public static hasNotProduct(productName: string): Cypress.Chainable<any> {
+    cy.get('apto-product-list').should('exist');
+
+    RequestHandler.registerInterceptions([Queries.FindProductsByFilter]);
+
+    cy.get('apto-search').find('.search-field input').clear().type(productName);
+
+    return cy.wait(RequestHandler.getAliasesFromRequests([Queries.FindProductsByFilter])).then(() => {
+      cy.get('.product-wrapper').should('have.length', 0);
+    });
+  }
+
+  public static hasProductPreviewImage(selector: string) {
+    cy.get(selector).find('img').should((img) => {
+      Core.isImageLoadedCheck(img);
+    });
+  }
+
+  public static hasTitle(selector: string, title: string|null = null) {
+    if (title) {
+      cy.get(selector).find('.product-description').find('h3').should('contain.text', title);
+    } else {
+      cy.get(selector).find('.product-description').find('h3').should('not.be.empty');
+    }
+  }
+
+  public static hasDescription(selector: string, description: string|null = null) {
+    if (description) {
+      cy.get(selector).find('.product-description').find('.description').should('contain.text', description);
+    } else {
+      cy.get(selector).find('.product-description').find('.description').should('not.be.empty');
+    }
+  }
+
+  /**
+   * check that link for the product is clickable and exist
+   *
+   * @param selector
+   */
+  public static isLinkOk(selector: string) {
+    const baseUrl = Cypress.env('baseUrl');
+
+    cy.get(selector)
+      .find('.product-description button')
+      .invoke('attr', 'data-link')
+      .then((link) => {
+        cy.wrap(link).should('exist');
+        cy.wrap(link).should('not.be.empty');
+
+        Core.isLinkBrokenTest(`${baseUrl}#${link}`);
+      });
   }
 }
