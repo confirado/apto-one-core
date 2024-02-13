72c3804f0a36f39f461cb1308e75d6f4722ee004
Arto Avagyan <arto.avagyan80@gmail.com>
2024-02-13 18:29:26 +0400

    tests/apto-one-core adds some methods to table.ts changes some existing ones

--- Tests/cypress/classes/common/elements/table.ts
+++ Tests/cypress/classes/common/elements/table.ts
@@ -21,29 +21,26 @@ export class Table {
    * provide column number for fast search
    *
    * @param text value to find in table
-   * @param column provide column number ofr fast search
    */
-  public static hasValue(text: string, column: number | null = null): typeof Table {
+  public static hasValue(text: string): typeof Table {
     cy.get(Table.initialSelector).should('exist').within(() => {
       cy.get('table tbody').within(() => {
         let found = false;
 
-        if (column === null) {
-          cy.get('tr').each($tr => {
-            cy.wrap($tr).find('td').each($td => {
-              const cellValue = $td.text().trim();
+        cy.get('tr').each($tr => {
+          // cy.wrap($tr).scrollIntoView();
 
-              if (cellValue.includes(text)) {
-                found = true;
-                return false; // means break the loop if found
-              }
-            });
-          }).then(() => {
-            expect(found).to.be.true;
+          cy.wrap($tr).find('td').each($td => {
+            const cellValue = $td.text().trim();
+
+            if (cellValue.includes(text)) {
+              found = true;
+              return false; // means break the loop if found
+            }
           });
-        } else {
-          cy.get('td:nth-child(' + column + ')').should('contain.text', text);
-        }
+        }).then(() => {
+          expect(found).to.be.true;
+        });
       });
     });
 
@@ -54,30 +51,53 @@ export class Table {
    * Table should not contain the given value
    *
    * @param text
-   * @param column
    */
-  public static hasNotValue(text: string, column: number | null = null): typeof Table {
+  public static hasNotValue(text: string): typeof Table {
     cy.get(Table.initialSelector).should('exist').within(() => {
       cy.get('table tbody').within(() => {
         let found = false;
 
-        if (column === null) {
-          cy.get('tr').each($tr => {
-            cy.wrap($tr).find('td').each($td => {
-              const cellValue = $td.text().trim();
+        cy.get('tr').each($tr => {
+          cy.wrap($tr).find('td').each($td => {
+            const cellValue = $td.text().trim();
 
-              if (cellValue.includes(text)) {
-                found = true;
-                return false; // means break the loop if found
-              }
-            });
-          }).then(() => {
-            expect(found).to.be.false;
+            if (cellValue.includes(text)) {
+              found = true;
+              return false; // means break the loop if found
+            }
           });
-        } else {
-          cy.get('td:nth-child(' + column + ')').should('contain.text', text);
-        }
+        }).then(() => {
+          expect(found).to.be.false;
+        });
       });
+    });
+
+    return Table;
+  }
+
+  /**
+   * Clicks one of the action button in table's right side
+   *
+   * searches the row with the given unique text and if finds clicks the action button int ath row
+   *
+   * @param type
+   * @param uniqueValue
+   * @param selector
+   */
+  public static clickAction(type: string, uniqueValue: any, selector: string): typeof Table{
+    cy.get(Table.initialSelector).should('exist').within(() => {
+      cy.get('table tbody').within(() => {
+        cy.get('tr').each($tr => {
+          cy.wrap($tr).find('td').each($td => {
+            const cellValue = $td.text().trim();
+
+            if (cellValue.includes(uniqueValue)) {
+              cy.wrap($tr).find('td:last-child').find(selector).click();
+              return false;
+            }
+          });
+        })
+       })
     });
 
     return Table;
