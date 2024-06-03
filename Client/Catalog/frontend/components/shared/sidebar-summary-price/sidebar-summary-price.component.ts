19f0617d673ec7850253d5c20174f6afaa0ee95f
Jan Neubert <j.neubert@confirado.de>
2024-06-03 09:20:48 +0200

    prepare for inline

--- Client/Catalog/frontend/components/shared/sidebar-summary-price/sidebar-summary-price.component.ts
+++ Client/Catalog/frontend/components/shared/sidebar-summary-price/sidebar-summary-price.component.ts
@@ -1,10 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { selectContentSnippet } from '@apto-base-frontend/store/content-snippets/content-snippets.selectors';
 import { Product } from '@apto-catalog-frontend/store/product/product.model';
-import {select, Store} from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { FormControl, FormGroup } from '@angular/forms';
-import { configurationIsValid } from '@apto-catalog-frontend/store/configuration/configuration.selectors';
+import {
+  configurationIsValid,
+  selectConfiguration,
+  selectCurrentPerspective
+} from '@apto-catalog-frontend/store/configuration/configuration.selectors';
 import { environment } from '@apto-frontend/src/environments/environment';
+import { addToBasket } from '@apto-catalog-frontend/store/configuration/configuration.actions';
+import { RenderImageService } from '@apto-catalog-frontend/services/render-image.service';
+import { BasketService } from '@apto-base-frontend/services/basket.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { untilDestroyed } from '@ngneat/until-destroy';
 
 @Component({
 	selector: 'apto-sidebar-summary-price',
@@ -23,16 +32,79 @@ export class SidebarSummaryPriceComponent {
 	@Input()
 	public discount: number | undefined;
 
+  public renderImage = null;
+  public specialCase = false;
+
   public quantityInputGroup = new FormGroup({
     quantityInput: new FormControl<number>(1),
   });
 
+  public configuration$ = this.store.select(selectConfiguration);
+
 	public readonly contentSnippets$ = this.store.select(selectContentSnippet('aptoSummary'));
   public readonly sidebarSummary$ = this.store.select(selectContentSnippet('sidebarSummary'));
   public readonly configurationIsValid$ = this.store.select(configurationIsValid);
-	public constructor(private store: Store) {}
+  public readonly isInline = !!environment.aptoInline;
+
+	public constructor(
+    private store: Store,
+    private renderImageService: RenderImageService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute)
+  {
+    this.configuration$.subscribe((c) => {
+      if (!c.loading && c.state.sections.length === 0) {
+        router.navigate(['..'], { relativeTo: activatedRoute });
+      }
+    });
+
+    this.store.select(selectCurrentPerspective).pipe(untilDestroyed(this)).subscribe(async (result: string) => {
+      this.renderImage = await this.renderImageService.drawImageForPerspective(result);
+    });
+  }
+
+  private get configurationId(): string {
+    return this.activatedRoute.snapshot.params['configurationId'];
+  }
+
+  private get configurationType(): string {
+    return this.activatedRoute.snapshot.params['configurationType'];
+  }
 
   protected get showPrices(): boolean {
     return environment.showPrices;
+  }
+
+  public openShopware6Cart() {
+    if (this.renderImage) {
+      this.renderImageService.resize(this.renderImage, 800).then((image: any) => {
+        this.store.dispatch(
+          addToBasket({
+            payload: {
+              type: 'ADD_TO_BASKET',
+              productImage: image.src,
+              configurationId: this.configurationId,
+              configurationType: this.configurationType,
+            },
+          })
+        );
+      });
+    } else {
+      this.store.dispatch(
+        addToBasket({
+          payload: {
+            type: 'ADD_TO_BASKET',
+            productImage: null,
+            configurationId: this.configurationId,
+            configurationType: this.configurationType,
+          },
+        })
+      );
+    }
+
+    const offCanvasCartInstances: any = window.PluginManager.getPluginInstances('OffCanvasCart');
+    for (let i = 0; i < offCanvasCartInstances.length; i++) {
+      offCanvasCartInstances[i].openOffCanvas(window.router['frontend.cart.offcanvas'], false);
+    }
   }
 }
