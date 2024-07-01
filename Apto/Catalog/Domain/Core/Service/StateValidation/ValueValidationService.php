cb3eaf3a39f9b5bee7f101b6ab97f9f97fa6c9e0
ronnybaumann <ronnybaumann@users.noreply.github.com>
2024-07-01 13:00:42 +0200

    filter out materials on state change that are not matching its conditions anymore

--- Apto/Catalog/Domain/Core/Service/StateValidation/ValueValidationService.php
+++ Apto/Catalog/Domain/Core/Service/StateValidation/ValueValidationService.php
@@ -4,11 +4,45 @@ namespace Apto\Catalog\Domain\Core\Service\StateValidation;
 
 use Apto\Base\Domain\Core\Model\AptoUuid;
 use Apto\Base\Domain\Core\Model\InvalidUuidException;
+use Apto\Catalog\Application\Core\Query\Product\Condition\ProductConditionSetFinder;
+use Apto\Catalog\Application\Core\Service\ComputedProductValue\CircularReferenceException;
 use Apto\Catalog\Domain\Core\Factory\ConfigurableProduct\ConfigurableProduct;
+use Apto\Catalog\Domain\Core\Factory\RuleFactory\Rule\Condition;
+use Apto\Catalog\Domain\Core\Factory\RuleFactory\Rule\LinkOperator;
+use Apto\Catalog\Domain\Core\Factory\RuleFactory\Rule\Payload\RulePayloadFactory;
+use Apto\Catalog\Domain\Core\Factory\RuleFactory\RuleFactory;
 use Apto\Catalog\Domain\Core\Model\Configuration\State\State;
+use Apto\Plugins\MaterialPickerElement\Application\Core\Query\Pool\PoolFinder;
 
 class ValueValidationService
 {
+    /**
+     * @var PoolFinder
+     */
+    protected $poolFinder;
+
+    /**
+     * @var ProductConditionSetFinder
+     */
+    private ProductConditionSetFinder $productConditionSetFinder;
+
+    /**
+     * @var RulePayloadFactory
+     */
+    protected RulePayloadFactory $rulePayloadFactory;
+
+    /**
+     * @param PoolFinder $poolFinder
+     * @param ProductConditionSetFinder $productConditionSetFinder
+     * @param RulePayloadFactory $rulePayloadFactory
+     */
+    public function __construct(PoolFinder $poolFinder, ProductConditionSetFinder $productConditionSetFinder, RulePayloadFactory $rulePayloadFactory)
+    {
+        $this->poolFinder = $poolFinder;
+        $this->productConditionSetFinder = $productConditionSetFinder;
+        $this->rulePayloadFactory = $rulePayloadFactory;
+    }
+
     private function getSectionListGroupedBy(State $state, string $fieldName): array
     {
         $sectionList = [];
@@ -56,6 +90,7 @@ class ValueValidationService
      *
      * @return void
      * @throws InvalidUuidException
+     * @throws CircularReferenceException
      */
     public function assertValidValues(ConfigurableProduct $product, State $state): void
     {
@@ -94,6 +129,101 @@ class ValueValidationService
                 }
             }
         }
+
+        // filter out materials by its conditions
+        $this->filterOutMaterialsByCondition($product, $state);
+    }
+
+    /**
+     * @param ConfigurableProduct $product
+     * @param State $state
+     * @return void
+     * @throws InvalidUuidException
+     * @throws CircularReferenceException
+     */
+    private function filterOutMaterialsByCondition(ConfigurableProduct $product, State $state)
+    {
+        $rulePayload = $this->rulePayloadFactory->getPayload($product, $state);
+        foreach ($state->getStateWithoutParameters() as $element) {
+            if (!(array_key_exists('aptoElementDefinitionId', $element['values']) && $element['values']['aptoElementDefinitionId'] === 'apto-element-material-picker')) {
+                continue;
+            }
+
+            $poolId = $element['values']['poolId'];
+
+            $items = $this->poolFinder->findPoolItemsFiltered($poolId, ['colorRating' => null, 'priceGroup' => null, 'properties' => []]);
+            $itemsMatchingCondition = [];
+
+            foreach ($items['data'] as $item) {
+
+                if ($item['material']['conditionSets'] && !empty($item['material']['conditionSets'])) {
+
+                    $productConditionsResult = $this->productConditionSetFinder->findByIds($item['material']['conditionSets']);
+
+                    $counter = 0;
+                    foreach ($productConditionsResult['data'] as $key => $productCondition) {
+
+                        $criterion = new Condition(
+                            new LinkOperator($productCondition['conditionsOperator']),
+                            RuleFactory::criteriaFromArray($productCondition['conditions'])
+                        );
+
+                        if ($criterion->isFulfilled($state, $rulePayload)) {
+                            $counter++;
+                        }
+                    }
+
+                    if ($counter >= count($productConditionsResult['data'])) {
+                        $itemsMatchingCondition[] = $item['material']['id'];
+                    }
+
+                } else {
+                    $itemsMatchingCondition[] = $item['material']['id'];
+                }
+            }
+
+            foreach ($this->getUsedMaterialIds($element['values']) as $materialId) {
+                if (!in_array($materialId, $itemsMatchingCondition)) {
+                    $state->removeElement(
+                        new AptoUuid($element['sectionId']),
+                        new AptoUuid($element['elementId']),
+                        $element['repetition']
+                    );
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * @param array $elementValues
+     * @return array
+     */
+    private function getUsedMaterialIds(array $elementValues): array
+    {
+        $materialIds = [];
+
+        if ($elementValues['materialId']) {
+            $materialIds[] = $elementValues['materialId'];
+        }
+
+        if ($elementValues['materialIdSecondary']) {
+            $materialIds[] = $elementValues['materialIdSecondary'];
+        }
+
+        if (array_key_exists('materials', $elementValues)) {
+            foreach ($elementValues['materials'] as $material) {
+                $materialIds[] = $material['id'];
+            }
+        }
+
+        if (array_key_exists('materialsSecondary', $elementValues)) {
+            foreach ($elementValues['materialsSecondary'] as $materialSecondary) {
+                $materialIds[] = $materialSecondary['id'];
+            }
+        }
+
+        return $materialIds;
     }
 
     /**
