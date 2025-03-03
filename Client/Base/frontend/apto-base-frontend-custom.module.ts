d0490dae06aad206d6d08e631216f0fb081bbb2e
Nihad Karajko <nihad.karajko@nanologika.de>
2025-03-03 17:23:21 +0100

    Implement MisterpenPrintingTechnologyComponent

--- Client/Base/frontend/apto-base-frontend-custom.module.ts
+++ Client/Base/frontend/apto-base-frontend-custom.module.ts
@@ -10,17 +10,22 @@ import {
 } from '@apto-one-template-frontend/components/misterpen-color-elements/misterpen-color-elements.component';
 import { AptoBaseCoreModule } from '@apto-base-core/apto-base-core.module';
 import { AsyncPipe, NgClass, NgForOf, NgIf } from '@angular/common';
+import {
+  MisterpenPrintingTechnologyComponent
+} from '@apto-one-template-frontend/components/misterpen-printing-technology/misterpen-printing-technology.component';
 
 @NgModule({
   declarations: [
     MisterpenHeaderComponent,
     MisterpenToolbarBottomComponent,
     MisterpenColorElementsComponent,
+    MisterpenPrintingTechnologyComponent
   ],
   exports: [
     MisterpenHeaderComponent,
     MisterpenToolbarBottomComponent,
     MisterpenColorElementsComponent,
+    MisterpenPrintingTechnologyComponent
   ],
   imports: [
     AptoBaseCoreModule,
