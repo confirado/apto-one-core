1ee23cb0be3744121c84155529f0a41180112570
Nihad Karajko <nihad.karajko@nanologika.de>
2025-12-16 15:01:55 +0100

    Rollback of area-element.component.ts

--- Client/Plugins/AreaElement/Client/frontend/components/area-element/area-element.component.ts
+++ Client/Plugins/AreaElement/Client/frontend/components/area-element/area-element.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { selectContentSnippet } from '@apto-base-frontend/store/content-snippets/content-snippets.selectors';
 import { SelectItem } from '@apto-catalog-frontend/models/select-items';
 import {
@@ -31,125 +31,61 @@ import { selectConfigurationError } from '@apto-catalog-frontend-configuration-s
 
 @UntilDestroy()
 @Component({
-	selector: 'apto-area-element',
-	templateUrl: './area-element.component.html',
-	styleUrls: ['./area-element.component.scss'],
+  selector: 'apto-area-element',
+  templateUrl: './area-element.component.html',
+  styleUrls: ['./area-element.component.scss'],
 })
 export class AreaElementComponent implements OnInit {
-	@Input()
-	public element: ProgressElement<AreaElementDefinitionProperties> | undefined | null;
+  @Input()
+  public element: ProgressElement<AreaElementDefinitionProperties> | undefined | null;
 
   @Input()
   public section: Section | undefined;
 
-	@Input()
-	public product: Product | null | undefined;
+  @Input()
+  public product: Product | null | undefined;
 
   @Input()
   public isDialog = false;
 
-	public formElement = new UntypedFormGroup({}, [this.sumValidator(true)]);
+  public formElement = new UntypedFormGroup({});
 
-	public readonly contentSnippet$ = this.store.select(selectContentSnippet('aptoDefaultElementDefinition'));
+  public readonly contentSnippet$ = this.store.select(selectContentSnippet('aptoDefaultElementDefinition'));
 
   public configurationError: ConfigurationError | null = null;
 
-	public itemFieldList: SelectItem[][] = [];
+  public itemFieldList: SelectItem[][] = [];
 
   public sumOfFieldValues = 0;
   public increaseStep: number | undefined;
   public decreaseStep: number | undefined;
 
-	public constructor(
+  public constructor(
     private store: Store,
     private dialogRef: MatDialogRef<AreaElementComponent>,
     private dialogService: DialogService,
     private readonly actions$: Actions
   ) {}
 
-	public getSelectValues(min: number, max: number, step: number): SelectItem[] {
-		const items: SelectItem[] = [];
-		for (let i = min; i <= max; i += step) {
-			items.push({
-				surrogateId: '',
-				id: `${i}`,
-				name: { de_DE: `${i}` },
-				isDefault: false,
-				aptoPrices: [],
-			});
-		}
-
-		return items;
-	}
-
-
-  private rangeValidator(min: number, max: number, allowZero: boolean) {
-    return (control: FormControl) => {
-      const value = control.value;
-
-      if (value === null || value === undefined) return null;
-      if (allowZero && value === 0) return null;
-      if (value < min || value > max) return { rangeError: true };
-
-      return null;
-    };
-  }
-
-  private sumValidator(allowZero: boolean) {
-    return (element) => {
-      if (!element.controls) return null;
-      if (Object.values(element.controls).length === 0) return null;
-
-      const value: number = this.getTotalValue();
-      if (allowZero && value === 0) return null;
-      if (value < this.getMinimumValue() || value > this.getMaximumValue()) return { rangeError: true };
-
-      return null;
-    };
-  }
-
-
-  private getTotalValue(): number {
-    let total: number = 0;
-
-    for (let c of Object.values(this.formElement.controls)) {
-      if (c.value && typeof(c.value) === 'number') {
-        total += Number(c.value);
-      }
+  public getSelectValues(min: number, max: number, step: number): SelectItem[] {
+    const items: SelectItem[] = [];
+    for (let i = min; i <= max; i += step) {
+      items.push({
+        surrogateId: '',
+        id: `${i}`,
+        name: { de_DE: `${i}` },
+        isDefault: false,
+        aptoPrices: [],
+      });
     }
 
-    return total;
+    return items;
   }
 
-  private getMinimumValue(): number {
-    for (let [key, value] of Object.entries(this.element.element.definition.properties)) {
-      if (key === 'sumOfFieldValue') {
-        if (value[0].minimum) {
-          return value[0].minimum;
-        }
-      }
+  public ngOnInit(): void {
+    if (!this.element) {
+      return;
     }
-
-    return 0;
-  }
-
-  private getMaximumValue(): number {
-    for (let [key, value] of Object.entries(this.element.element.definition.properties)) {
-      if (key === 'sumOfFieldValue') {
-        if (value[0].maximum) {
-          return value[0].maximum;
-        }
-      }
-    }
-
-    return 0;
-  }
-
-
-	public ngOnInit(): void {
-		if (!this.element) {
-			return;
-		}
 
     this.store.select(selectConfigurationError).subscribe((next) => {
       this.configurationError = next;
@@ -157,53 +93,44 @@ export class AreaElementComponent implements OnInit {
 
     this.initIncreaseDecreaseStep();
 
-		for (
-			let i = 0;
-			i < Object.entries(this.element.element.definition.properties).filter(([property]) => property.includes('field_')).length;
-			i++
-		) {
-			let itemsField: SelectItem[] = [];
+    for (
+      let i = 0;
+      i < Object.entries(this.element.element.definition.properties).filter(([property]) => property.includes('field_')).length;
+      i += 1
+    ) {
+      let itemsField: SelectItem[] = [];
       let validators = [];
 
       if (this.element.element.definition.staticValues.fields?.[i]?.rendering === 'input') {
         validators = [
-          Validators.required
+          Validators.required,
         ];
+      }
 
-        for (let index = 0; index < Object.entries(this.element.element.definition.properties[`field_${i}`]).length; index++) {
-          const min: number = this.element.element.definition.properties[`field_${i}`][index].minimum;
-          const max: number = this.element.element.definition.properties[`field_${i}`][index].maximum;
+      this.formElement.addControl(
+        `field_${i}`,
+        new UntypedFormControl(
+          this.element.state.values[`field_${i}`] || this.element.element.definition.staticValues.fields?.[i]?.default || 0,
+          validators
+        )
+      );
 
-          if (min || max) {
-            validators.push(this.rangeValidator(min, max, true));
+      if (this.element.element.definition.staticValues.fields?.[i]?.rendering === 'select') {
+        for (let index = 0; index < Object.entries(this.element.element.definition.properties[`field_${i}`]).length; index += 1) {
+          let itemField: SelectItem[] = [];
+          if (this.element.element.definition.properties[`field_${i}`][index].maximum) {
+            itemField = this.getSelectValues(
+              this.element.element.definition.properties[`field_${i}`][index].minimum,
+              this.element.element.definition.properties[`field_${i}`][index].maximum,
+              this.element.element.definition.properties[`field_${i}`][index].step
+            );
           }
+          itemsField = itemsField.concat(itemField);
         }
       }
 
-			this.formElement.addControl(
-				`field_${i}`,
-				new UntypedFormControl(
-					this.element.state.values[`field_${i}`] || this.element.element.definition.staticValues.fields?.[i]?.default || 0,
-          validators
-				)
-			);
-
-			if (this.element.element.definition.staticValues.fields?.[i]?.rendering === 'select') {
-				for (let index = 0; index < Object.entries(this.element.element.definition.properties[`field_${i}`]).length; index++) {
-					let itemField: SelectItem[] = [];
-					if (this.element.element.definition.properties[`field_${i}`][index].maximum) {
-						itemField = this.getSelectValues(
-							this.element.element.definition.properties[`field_${i}`][index].minimum,
-							this.element.element.definition.properties[`field_${i}`][index].maximum,
-							this.element.element.definition.properties[`field_${i}`][index].step
-						);
-					}
-					itemsField = itemsField.concat(itemField);
-				}
-			}
-
-			this.itemFieldList.push(itemsField);
-		}
+      this.itemFieldList.push(itemsField);
+    }
 
     // todo add logic for not allowing go further when the sum is too big
     this.formElement.valueChanges.subscribe(x => {
@@ -219,13 +146,13 @@ export class AreaElementComponent implements OnInit {
   }
 
   public hasValues(): boolean {
-		return this.element ? this.element.state.active : false;
-	}
+    return this.element ? this.element.state.active : false;
+  }
 
-	public saveInput(): void {
-		if (!this.element) {
-			return;
-		}
+  public saveInput(): void {
+    if (!this.element) {
+      return;
+    }
 
     this.markAllControlsAsDirty();
 
@@ -245,39 +172,39 @@ export class AreaElementComponent implements OnInit {
     }
 
     this.closeModalOnSuccess();
-		this.store.dispatch(
-			updateConfigurationState({
-				updates: {
-					set: Object.entries(this.formElement.value).map(([property, value]) => ({
-						sectionId: this.element!.element.sectionId,
-						elementId: this.element!.element.id,
+    this.store.dispatch(
+      updateConfigurationState({
+        updates: {
+          set: Object.entries(this.formElement.value).map(([property, value]) => ({
+            sectionId: this.element!.element.sectionId,
+            elementId: this.element!.element.id,
             sectionRepetition: this.element!.state.sectionRepetition,
             property,
-						value,
-					})),
-				},
-			})
-		);
-	}
+            value,
+          })),
+        },
+      })
+    );
+  }
 
-	public removeInput(): void {
-		if (!this.element) {
-			return;
-		}
-		this.store.dispatch(
-			updateConfigurationState({
-				updates: {
-					remove: Object.entries(this.formElement.value).map(([property, value]) => ({
-						sectionId: this.element!.element.sectionId,
-						elementId: this.element!.element.id,
+  public removeInput(): void {
+    if (!this.element) {
+      return;
+    }
+    this.store.dispatch(
+      updateConfigurationState({
+        updates: {
+          remove: Object.entries(this.formElement.value).map(([property, value]) => ({
+            sectionId: this.element!.element.sectionId,
+            elementId: this.element!.element.id,
             sectionRepetition: this.element!.state.sectionRepetition,
             property,
-						value,
-					})),
-				},
-			})
-		);
-	}
+            value,
+          })),
+        },
+      })
+    );
+  }
 
   public closeModal(): void {
     this.dialogRef.close();
